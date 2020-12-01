a9d44cd14b9ab034d8f85fc803cdbb426d87c688
Dumidu Pabasara <eagleshadowtrigono@gmail.com>
2020-12-01 14:27:23 +0530

    home_screen and courses basic ui design done

--- src/components/home/courses/index.js
+++ src/components/home/courses/index.js
@@ -1,8 +1,72 @@
 import React from 'react';
 import { ScrollView, Text, Button } from 'react-native';
+import { ListItem, Avatar, Card} from 'react-native-elements'
+
+/*const list = [
+    {
+      name: 'CSC1111',
+      avatar_url: '../../../images/green.jpg',
+      subtitle: 'Programming',
+      time:'8-10 AM'
+    },
+    {
+      name: 'CSC2122',
+      avatar_url: '../../../images/red.jpg',
+      subtitle: 'Mobile Development',
+      time:'10-12 AM'
+    },
+    
+  ];*/
 
 
 const Courses = ({ navigation }) => {
+
+    return(
+        <ScrollView>
+            <Card>
+                <Card.FeaturedSubtitle style={{ color: '#1e90ff', justifyContent: 'center' }}>Your Courses</Card.FeaturedSubtitle>
+                <Card.Divider/>
+                {
+                    /*list.map((l, i) => (*/}
+                    <ListItem 
+                        bottomDivider
+                        onPress={()=> navigation.navigate('QRScanner_screen')}
+                    >
+                        <Avatar 
+                            size={50}
+                            overlayContainerStyle={{backgroundColor: 'green'}}
+                            rounded title='8' 
+                            activeOpacity={0.7}
+                        />
+                        <ListItem.Content>
+                            <ListItem.Title>CSC1111</ListItem.Title>
+                            <ListItem.Subtitle>Programming</ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                    <ListItem 
+                    bottomDivider
+                    onPress={()=> navigation.navigate('QRScanner_screen')}
+                >
+                    <Avatar 
+                        size={50}
+                        overlayContainerStyle={{backgroundColor: 'red'}}
+                        rounded title='10' 
+                        activeOpacity={0.7}
+                    />
+                    <ListItem.Content>
+                        <ListItem.Title>CSC1133</ListItem.Title>
+                        <ListItem.Subtitle>Mobile Development</ListItem.Subtitle>
+                    </ListItem.Content>
+                </ListItem>
+                   {/* ))*/
+                }
+            </Card>
+        </ScrollView>
+    )
+}
+
+
+/*const Courses = ({ navigation }) => {
 
     return(
         <ScrollView>
@@ -25,6 +89,7 @@ const Courses = ({ navigation }) => {
             />
         </ScrollView>
     )
-}
+}*/
 
 export default Courses;
+
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { Card } from 'react-native-elements';
 import Courses from './courses';
 
 const HomeScreen = ({navigation}) => {
 
     return(
-        <View>
-            <Text>Hello sc/2017/10266 Pabasara</Text>
+        <View style={styles.mainBody}>
+            <Card>
+                <Card.FeaturedTitle style={{ color: '#1e90ff' }}>Hello sc/2017/10266 Pabasara</Card.FeaturedTitle>
+            </Card>
             <Courses navigation={navigation}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+
+    mainBody: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#e0ffff',
+        alignContent: 'center',
+    },
+    
+});
 
 export default HomeScreen;
