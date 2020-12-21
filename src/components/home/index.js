644dfe0b188465b635c7f2f06b80645981556d33
Dumidu Pabasara <eagleshadowtrigono@gmail.com>
2020-12-21 10:59:53 +0530

    Refresh Controller add to home screen

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import { ScrollView, Text, StyleSheet } from 'react-native';
+import React , { useState } from 'react';
+import { ScrollView, Text, StyleSheet, RefreshControl } from 'react-native';
 import { Card } from 'react-native-elements';
 import Courses from './courses';
 import * as Animatable from 'react-native-animatable';
 import { useSelector } from 'react-redux';
 import { getLoggedInUserDetails } from '../../store/login';
 import { LinearGradient }  from 'expo-linear-gradient';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Constants from 'expo-constants';
+
+const wait = (timeout) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout);
+  });
+}
 
 const HomeScreen = ( {navigation} ) => {
 
   const user = useSelector(getLoggedInUserDetails);
 
-  /*const Courses = React.forwardRef(({navigation}, ref) => (
+  const [refreshing, setRefreshing] = React.useState(false);
 
-    <Courses navigation={ navigation } user={ user } ref={ref}/>
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
 
-  ));*/
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
 
   return (
     <LinearGradient
@@ -23,7 +33,12 @@ const HomeScreen = ( {navigation} ) => {
       start={[0.1, 0.1]}
       style={styles.mainBody}
     >
-      <ScrollView >
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        } 
+      >
         <Animatable.View animation="slideInDown" duration={ 1500 } delay={ 500 }>
           <Card>
             <Card.FeaturedTitle style={ { color: '#1e90ff' } }>Hello { user.firstName } { user.username }</Card.FeaturedTitle>
@@ -42,6 +57,10 @@ const styles = StyleSheet.create({
   mainBody: {
     flex: 1,
     alignContent: 'center'
+  },
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
   },
 
 });
